2177f478d9a4fa2c657bb4bdd2c83aed5ecfe8ac
MelinaZarateB <melinazarate77@gmail.com>
2024-12-11 10:12:35 -0300

    Preparando para produccion

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,5 @@
 import "./Users.css";
 import { useState, useEffect } from "react";
-import CreateUser from "../CreateUser/CreateUser";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../../redux/actions/userActions";
 import AdminUsers from "../AdminUsers/AdminUsers";
@@ -10,9 +9,9 @@ const Users = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.user.users);
   const userByEmail = useSelector((state) => state.user.userByEmail);
-  const stateRegisterUser = useSelector((state) => state.user.registerUser)
-  const deleteUser = useSelector((state) => state.user.deleteUserSuccess)
-  
+  const stateRegisterUser = useSelector((state) => state.user.registerUser);
+  const deleteUser = useSelector((state) => state.user.deleteUserSuccess);
+
   useEffect(() => {
     dispatch(getAllUsers());
   }, [stateRegisterUser, deleteUser]);
@@ -25,11 +24,7 @@ const Users = () => {
   };
   return (
     <section className="container-users">
-        {createUser ? (
-          <CreateUser handleCreateUser={handleCreateUser} />
-        ) : (
-          <AdminUsers handleCreateUser={handleCreateUser} />
-        )}
+      <AdminUsers handleCreateUser={handleCreateUser} />
     </section>
   );
 };
